e5e6d607f2e30f435e73eb735d4692dc6c8f2c0a
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-04-11 23:21:35 +0300

    defaine method to handle validation step in requests

--- app/Core/Helper/Validator.php
+++ app/Core/Helper/Validator.php
@@ -10,7 +10,9 @@ class Validator
     public static $rules = [
         'required' => Required::class,
     ];
-    public static function validate($rule, $errorKey, $data)
+
+
+    public static function checkRule($rule, $errorKey, $data)
     {
         $valid_rule = self::$rules[$rule] ?? false;
        
@@ -23,6 +25,33 @@ class Validator
             }
         } else {
             throw new Exception("Rule not found");
+        }
+    }
+
+
+    
+    /**
+     * Validates the given request data against the given rules.
+     *
+     * @param array $requestdata The request data to validate.
+     * @param array $rules The rules to validate the request data against.
+     *
+     * @throws Exception If the request data does not contain a key specified in the rules.
+     *
+     * @return mixed If any validation fails, returns a redirect response to the previous page.
+     *               If all validation succeeds, returns nothing.
+     */
+    public static function validate($requestdata, $rules)
+    {
+        foreach ($rules as $errorKey => $ruleList) {
+            if (!array_key_exists($errorKey, $requestdata)) {
+                throw new Exception("InvalidParameter: key not found in request data");
+            }
+            foreach ($ruleList as $rule) {
+                if (!self::checkRule($rule, $errorKey, $requestdata[$errorKey])) {
+                    return redirect(back());
+                }
+            }
         }
     }
 }
