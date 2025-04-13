cc2a7c3d9efb5ecbe4c6755230406e0e11e571f6
Raiez Alshamaly <raiez.shamaly@gmail.com>
2025-04-13 05:40:34 +0300

    fix navbar

--- recources/views/layouts/header.php
+++ recources/views/layouts/header.php
@@ -65,3 +65,6 @@ use App\Core\Helper\Session;
             </div>
         </div>
     </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
